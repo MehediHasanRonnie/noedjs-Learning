9a03105eb5842828cdb64de41ca011f313cb5208
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-12-19 21:43:01 +0600

    commited

---
+++ exportRequire/index.js
@@ -0,0 +1,9 @@
+// const s1 = require("./students");
+
+// console.log(s1.getName());
+// console.log(s1.getAge());
+// console.log(s1.result);
+
+const { getName } = require("./students");
+
+console.log(getName());
---
+++ exportRequire/students.js
@@ -0,0 +1,20 @@
+exports.getName = () => {
+  return "Mehedi hasan";
+};
+
+const getAge = () => {
+  return "25";
+};
+const result = 3.83;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = result;
+
+//to send somthing at a time we have to use the function bellow
+
+// module.exports = {
+//   getAge,
+//   getName,
+//   result,
+// };
